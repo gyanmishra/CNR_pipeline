7b5e62125cb4720d7c8af861a917e91123e9f070
gyanmishra <j12mishra@gmail.com>
2024-09-04 14:17:16 -0500

    first commit

---
+++ macs2_callPeak.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl
+
+use POSIX qw(strftime);
+use FindBin;
+use Cwd 'abs_path';
+
+# Get the absolute path of the current script
+my $script_path = abs_path($FindBin::Bin);
+#print "$script_path\n";
+
+# Project Directory
+$outputDir = $ARGV[0]; 
+#print "$projectDir\n";
+
+# pvalue Directory
+$pvalue = $ARGV[1];
+#print "$outputDir\n";
+
+# results directory
+unless (-e "$outputDir/macs2_callPeak") {
+    `mkdir -p $outputDir/macs2_callPeak`;
+}
+unless (-e "$outputDir/logs") {
+    `mkdir -p $outputDir/logs`;
+}
+
+# provide a tab separated file having three column 
+# 1st column : peakCallingID 
+# 2nd column : targetFiles 
+# 3rd column : controlFiles
+
+$sampleData  = $ARGV[2];
+open my $file, $sampleData or die "Could not open $sampleData: $!";
+
+my $first = 1;
+foreach $line (<$file>) 
+{
+       chomp($line);
+       if( $first ) {
+            $first = 0;
+        }
+        else {
+            chomp($line);
+            @col = split('\t',$line);
+            chomp($col[0]);
+            chomp($col[1]);
+            chomp($col[2]);
+
+            @target = split(',',$col[1]);
+            @control = split(',',$col[2]);
+            $t_length = scalar @target;
+            $c_length = scalar @control;
+
+            print "$t_length\n";
+            @targets =[];
+            @controls =[];
+
+            for($i=0;$i<$t_length;$i++){
+                $targets[$i] = $outputDir . "/bam/" . $target[$i] . '.final.sort.bam';
+            }
+            for($i=0;$i<$c_length;$i++){
+                $controls[$i] = $outputDir . "/bam/" . $control[$i] . '.final.sort.bam';
+            }
+
+            print "@controls\n";
+            print "@targets\n";
+
+            $sampleName = $col[0];
+            print "$sampleName\n";
+            #$sampleName =~ s/\s/_/g;
+            $out_filename = $outputDir . "/logs/" . $sampleName . '_macs2.sh';      
+            print "$out_filename\n";
+            #print "$sampleName\n";
+            open(FH, '>', $out_filename) or die $!;
+            print FH "#!/bin/bash\n\n";
+            print FH "#SBATCH --job-name $sampleName\n";
+            print FH "#SBATCH -N 1\n";
+            print FH "#SBATCH --cpus-per-task=32\n";
+            print FH "#SBATCH --partition=32GB\n";
+            print FH "#SBATCH -t 0-12:0:0\n";
+            print FH "#SBATCH -o $outputDir/logs/$col[0]_macs2.out\n";
+            print FH "#SBATCH -e $outputDir/logs/$col[0]_macs2.err\n";
+            print FH "#SBATCH --mail-type ALL\n";
+            #print FH "#SBATCH --mail-user GyanPrakash.Mishra\@UTSouthwestern.edu\n\n\n";
+            print FH "module load macs/2.1.2 bedops/2.4.14 \n\n";
+            #print FH "perl /work/OBI/Neuroinformatics_Core/s225347/CUT_and_RUN/scripts/CNR_pipeline.pl /work/OBI/Neuroinformatics_Core/s225347/CUT_and_RUN/data/CUTandRUN_seq_Data.txt\n\n";
+            print FH "\n\n";
+            print FH "# peak calling \n";         
+            # Merge samples fa          
+            if($pvalue eq 'pvalue'){
+              print FH "macs2 callpeak \\\n-t @targets \\\n-c @controls \\\n-f BAMPE \\\n-g mm \\\n-p 1e-3 \\\n--outdir $outputDir/macs2_callPeak/$sampleName \\\n--call-summits \\\n-n $sampleName\n\n";
+              print FH "#remove blacklisted region\n";
+              print FH "bedops -n 1 $outputDir/macs2_callPeak/$sampleName/${sampleName}_peaks.narrowPeak \\\nmm10/mm10.blacklist.bed >$outputDir/macs2_callPeak/$sampleName/$sampleName_peaks.narrowPeak.noBlacklist.bed\n\n";
+              print FH "bedops -n 1 $outputDir/macs2_callPeak/$sampleName/${sampleName}_summits.bed \\\nmm10/mm10.blacklist.bed >$outputDir/macs2_callPeak/$sampleName/$sampleName_summits_noBlacklist.bed\n";
+            }
+
+            if($pvalue eq 'qvalue'){
+              print FH "macs2 callpeak \\\n-t @targets \\\n-c @controls \\\n-f BAM \\\n-g mm \\\n--outdir $outputDir/macs2_callPeak/$sampleName \\\n--call-summits \\\n-n $sampleName\n\n";
+
+            print FH "#remove blacklisted region\n";
+            print FH "bedops -n 1 $outputDir/macs2_callPeak/$sampleName/${sampleName}_peaks.narrowPeak \\\n$script_path/mm10/mm10.blacklist.bed >$outputDir/macs2_callPeak/$sampleName/$sampleName_peaks.narrowPeak.noBlacklist.bed\n\n";
+            print FH "bedops -n 1 $outputDir/macs2_callPeak/$sampleName/${sampleName}_summits.bed \\\n$script_path/mm10/mm10.blacklist.bed >$outputDir/macs2_callPeak/$sampleName/$sampleName_summits_noBlacklist.bed\n";
+            }
+            `sbatch $out_filename`;  
+    }
+}
+            
---
+++ process_CNR.pl
@@ -0,0 +1,149 @@
+#!/usr/bin/perl
+
+use POSIX qw(strftime);
+use FindBin;
+use Cwd 'abs_path';
+
+# Get the absolute path of the current script
+my $script_path = abs_path($FindBin::Bin);
+print "$script_path\n";
+
+# Project Directory
+$outputDir = $ARGV[0]; 
+print "$outputDir\n";
+
+# Raw data direcetory
+$rawFile = $ARGV[1];
+print "$rawFile\n";
+
+# Index Directory
+$index = $ARGV[2];
+print "$index\n";
+
+# results directory
+unless (-e "$outputDir/fastqc_before_trimmomatic") {
+    `mkdir -p $outputDir/fastqc_before_trimmomatic`;
+}
+unless (-e "$outputDir/fastq") {
+    `mkdir -p $outputDir/fastq`;
+}
+unless (-e "$outputDir/fastqc") {
+    `mkdir -p $outputDir/fastqc`;
+}
+unless (-e "$outputDir/bam") {
+    `mkdir -p $outputDir/bam`;
+}
+unless (-e "$outputDir/qc_qualimap") {
+    `mkdir -p $outputDir/qc_qualimap`;
+}
+unless (-e "$outputDir/logs") {
+    `mkdir -p $outputDir/logs`;
+}
+unless (-e "$outputDir/bamCoverage") {
+    `mkdir -p $outputDir/bamCoverage`;
+}
+
+
+$datestring = strftime "%F", localtime;
+$datestring =~ s/-//g ; 
+
+#$Job_dir  = '$outputDir/logs/' . $datestring;
+
+#unless (-e $Job_dir) {
+#    `mkdir $Job_dir`;
+#}
+
+
+# provide a file having three column (1st column : 1st column as Sample name and 2nd and 3rd column : name of paired end fastq file (Read1 and Read2))
+$sampleData  = $ARGV[3];
+open my $file, $sampleData or die "Could not open $sampleData: $!";
+
+my $first = 1;
+foreach $line (<$file>) 
+{
+       chomp($line);
+       if( $first ) {
+            $first = 0;
+        }
+        else {
+              chomp($line);
+              @col = split(',',$line);
+              chomp($col[0]);
+              chomp($col[1]);
+              chomp($col[2]);
+
+              $sampleName = $col[0];
+              print "$sampleName\n";
+              #$sampleName =~ s/\s/_/g;
+              $out_filename = $outputDir . "/logs/" . $sampleName . '.sh';
+
+              print "$out_filename\n";
+              #print "$sampleName\n";
+              open(FH, '>', $out_filename) or die $!;
+              print FH "#!/bin/bash\n\n";
+              print FH "#SBATCH --job-name CUT_and_RUN\n";
+              print FH "#SBATCH -N 1\n";
+              print FH "#SBATCH --cpus-per-task=32\n";
+              print FH "#SBATCH --partition=32GB\n";
+              print FH "#SBATCH -t 0-12:0:0\n";
+              print FH "#SBATCH -o $outputDir/logs/$col[0].out\n";
+              print FH "#SBATCH -o $outputDir/logs/$col[0].err\n";
+              print FH "#SBATCH --mail-type ALL\n";
+              print FH "#SBATCH --mail-user GyanPrakash.Mishra\@UTSouthwestern.edu\n\n\n";
+              print FH "module load fastqc/0.11.8 multiqc/1.7 perl/5.30.1 cutadapt/2.5 bowtie2/2.4.2 samtools/1.6 bedtools/2.29.2 UCSC_userApps/v317 Trimmomatic/0.32 deeptools/3.5.0 \n\n";
+              #print FH "perl /work/OBI/Neuroinformatics_Core/s225347/CUT_and_RUN/scripts/CNR_pipeline.pl /work/OBI/Neuroinformatics_Core/s225347/CUT_and_RUN/data/CUTandRUN_seq_Data.txt\n\n";
+              print FH "\n\n";
+              print FH "# COMMAND GROUP 1\n\n";         
+              # Merge samples fastq
+              #print FH "cat $rawFile/$col[1]*_R1*fastq.gz >$projectDir/results/fastq/$col[0]_R1.fastq.gz\n";
+              #print FH "cat $rawFile/$col[1]*_R2*fastq.gz >$projectDir/results/fastq/$col[0]_R2.fastq.gz\n";
+              
+              ## FASTQC 
+              print FH "fastqc $rawFile/$col[1] \\\n-o $outputDir/fastqc_before_trimmomatic\n\n";
+              print FH "fastqc $rawFile/$col[2] \\\n-o $outputDir/fastqc_before_trimmomatic\n\n";
+
+              # MultiQC before trimmomatic
+              print FH "multiqc $outputDir/fastqc_before_trimmomatic -o $outputDir/fastqc_before_trimmomatic\n\n";
+
+              # Trim adapters
+              #print FH "cutadapt -a  AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATC -A  AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATC -m 20 -o $projectDir/results/fastq/$col[4]_R1.cutadapt.fastq.gz -p $projectDir/results/fastq/$col[4]_R2.cutadapt.fastq.gz $rawFile/$col[5] $rawFile/$col[6]\n";
+              #
+              print FH "java -jar $script_path/\$Trimmomatic PE  \\\n$rawFile/$col[1] \\\n$rawFile/$col[2] \\\n$outputDir/fastq/$sampleName.R1.paired.fastq.gz \\\n$outputDir/fastq/$sampleName.R1.unpaired.fastq.gz \\\n$outputDir/fastq/$sampleName.R2.paired.fastq.gz \\\n$outputDir/fastq/$sampleName.R2.unpaired.fastq.gz \\\nILLUMINACLIP:trimmomatic/adapters/TruSeq3-PE.fa:2:15:4:4:true LEADING:20 TRAILING:20 SLIDINGWINDOW:4:15 MINLEN:25\n\n";
+
+              ## FASTQC 
+              print FH "fastqc $outputDir/fastq/$sampleName.R1.paired.fastq.gz \\\n-o $outputDir/fastqc\n\n";
+              print FH "fastqc $outputDir/fastq/$sampleName.R2.paired.fastq.gz \\\n-o $outputDir/fastqc\n\n";
+
+              # MultiQC
+              print FH "multiqc $outputDir/fastqc -o $outputDir/fastqc\n\n";
+
+              ## Align to mm10 reference genome
+              print FH "bowtie2 -p 4 --local --very-sensitive-local --no-unal --no-mixed --no-discordant --phred33 -I 10 -X 700 \\\n-x $index/mm10 \\\n-1 $outputDir/fastq/$sampleName.R1.paired.fastq.gz \\\n-2 $outputDir/fastq/$sampleName.R2.paired.fastq.gz | samtools view -bS -  \\\n>$outputDir/bam/$sampleName.bam\n\n";
+              ##
+              print FH "samtools sort -@ 8 -n -O BAM \\\n$outputDir/bam/$sampleName.bam \\\n-o $outputDir/bam/$sampleName.s.bam\n\n";
+              print FH "samtools fixmate -m \\\n$outputDir/bam/$sampleName.s.bam \\\n$outputDir/bam/$sampleName.s.fm.bam\n\n";
+              print FH "samtools sort -@ 8 \\\n$outputDir/bam/$sampleName.s.fm.bam \\\n-o $outputDir/bam/$sampleName.s.fm.sort.bam\n\n";
+              ###
+              ### Mark Duplicates 
+              print FH "samtools markdup -r -s \\\n$outputDir/bam/$sampleName.s.fm.sort.bam \\\n$outputDir/bam/$sampleName.s.fm.sort.md.bam\n\n";
+              ##
+              print FH "samtools sort -@ 8 \\\n$outputDir/bam/$sampleName.s.fm.sort.md.bam \\\n-o $outputDir/bam/$sampleName.final.sort.bam\n\n";
+              ##
+              # Generate index
+              print FH "samtools index $outputDir/bam/$sampleName.final.sort.bam\n\n";
+
+              # Run QuanliMap
+              print FH "$script_path/qualimap_v2.3/qualimap bamqc \\\n-bam $outputDir/bam/$sampleName.final.sort.bam \\\n-outdir $outputDir/qc_qualimap/$sampleName \\\n--java-mem-size=4G\n\n";
+              
+              # Run bamCoverage 
+              print FH "bamCoverage --bam $outputDir/bam/$sampleName.final.sort.bam  -o $outputDir/bamCoverage/$sampleName.RPKM.bedGraph \\\n--binSize 10 \\\n--blackListFileName $script_path/mm10/mm10.blacklist_merged.bed \\\n--effectiveGenomeSize 2652783500 \\\n--normalizeUsing RPKM \\\n--outFileFormat \"bedgraph\" \\\n-p \"max/2\"\n\n";
+              print FH "bamCoverage --bam $outputDir/bam/$sampleName.final.sort.bam  -o $outputDir/bamCoverage/$sampleName.RPKM.bw \\\n--binSize 10 \\\n--blackListFileName $script_path/mm10/mm10.blacklist_merged.bed \\\n--effectiveGenomeSize 2652783500 \\\n--normalizeUsing RPKM \\\n--outFileFormat \"bigwig\" \\\n-p \"max/2\"\n\n";
+              
+              print FH "rm $outputDir/bam/$sampleName.bam\n";
+              print FH "rm $outputDir/bam/$sampleName.s.bam\n";
+              print FH "rm $outputDir/bam/$sampleName.s.fm.bam\n";
+              print FH "rm $outputDir/bam/$sampleName.s.fm.sort.bam\n";
+              print FH "rm $outputDir/bam/$sampleName.s.fm.sort.md.bam\n";
+              #`sbatch $out_filename`;
+       }
+}
