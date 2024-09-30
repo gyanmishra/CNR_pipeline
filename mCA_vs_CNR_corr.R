9fc3c00326f16edb5c1dd3e98097061af6ecb312
GYAN MISHRA <s225347@Nucleus004.cm.cluster>
2024-09-30 16:41:30 -0500

    corr updated

--- mCA_vs_CNR_corr.R
+++ mCA_vs_CNR_corr.R
@@ -1,25 +1,30 @@
 
 ########################################################################################################################
 # Prepare input files for 
+
 # methylation data
 mCA_target = "/work/OBI/Neuroinformatics_Core/s225347/DNMT3A_project/data/BSmap/2wk_CTX_Stroud2017_mm10_BSmap_CA.txt.gz"
 
 # CNR bam files (only one sample)
 CNR_target = "/project/OBI/Neuroinformatics_Core/Stroud_lab/shared/Eric_New/For_Gyan/210930_H4_S835A_MECP2.final.sort.bam"
 
-# multiple samples
+# multiple CNR samples
 CNR_target = list.files(path = "/project/OBI/Neuroinformatics_Core/Stroud_lab/shared/Eric_New/For_Gyan",
                         pattern = "210930",full.names = TRUE)
-CNR_target = c("/path1/file.bam","path2/file2.bam")
+
+# Also can be defined as 
+# CNR_target = c("/path1/file.bam","path2/file2.bam")
 
 # Mus musculus GTF file
 gtf = '/work/Neuroinformatics_Core/s225347/Pipeline/CNR_pipeline/Mus_musculus.GRCm38.102.gtf.gz'
 
-
+########################################################################################################################
+# Define bin and step size
 binSize <- 399
 stepSize <- 40
 
-# Function to check to is the requored packages is installed or not.
+########################################################################################################################
+# Function to check to is the required packages is installed or not.
 install_if_missing <- function(pkg) {
   if (!requireNamespace(pkg, quietly = TRUE)) {
     BiocManager::install(pkg)
@@ -64,25 +69,6 @@ summarize_logFC_mCA = function(x,binSize = 19,stepSize =4){
   }
 }
 
-
-
-# Read the GTF file
-gtf_data <- import(gtf, format = "gtf")
-protein_coding = gtf_data[mcols(gtf_data)$type == "gene"] %>% 
-  .[!(is.na(mcols(.)$gene_biotype))] %>% 
-  .[mcols(.)$gene_biotype == 'protein_coding'] %>% .[width(.) >4500]
-new_width = width(protein_coding) -3000
-protein_coding = resize(protein_coding, width = new_width, fix = ifelse(strand(protein_coding) == "+", "start", "end"))
-seqlevels(protein_coding) <- paste0("chr", seqlevels(protein_coding))
-
-
-GR.colnames = c('chr','start','end')
-mm10.blacklisted.Regions = read.csv(paste0(script_path,"/mm10/mm10.blacklist.bed"),sep="\t") %>% 
-  rename_with(~GR.colnames) %>% GRanges()
-
-protein_coding.noBlacklist = subsetByOverlaps(protein_coding, mm10.blacklisted.Regions, invert = TRUE)
-mcols(protein_coding.noBlacklist) <- NULL
-
 # Function to calculate RPKM from bam file
 bamFileRPKM =  function(bamfile){
   sample_name = basename(bamfile)
@@ -116,15 +102,37 @@ bamFileRPKM =  function(bamfile){
   return(gene.GR)
 }
 
+########################################################################################################################
+
+# Prepare the genomic coordinates of protein coding genes and filter out genes less that 4.5kb and also removes 
+# first 3kb of region from start of gene.
+gtf_data <- import(gtf, format = "gtf")
+protein_coding = gtf_data[mcols(gtf_data)$type == "gene"] %>% 
+  .[!(is.na(mcols(.)$gene_biotype))] %>% 
+  .[mcols(.)$gene_biotype == 'protein_coding'] %>% .[width(.) >4500]
+new_width = width(protein_coding) -3000
+protein_coding = resize(protein_coding, width = new_width, fix = ifelse(strand(protein_coding) == "+", "start", "end"))
+seqlevels(protein_coding) <- paste0("chr", seqlevels(protein_coding))
+
+
+GR.colnames = c('chr','start','end')
+mm10.blacklisted.Regions = read.csv(paste0(script_path,"/mm10/mm10.blacklist.bed"),sep="\t") %>% 
+  rename_with(~GR.colnames) %>% GRanges()
+
+protein_coding.noBlacklist = subsetByOverlaps(protein_coding, mm10.blacklisted.Regions, invert = TRUE)
+mcols(protein_coding.noBlacklist) <- NULL
+
+########################################################################################################################
+# apply the function to calculate the RPKM for each sample in CNR_target object
 bamList = list()
 
-# apply the function to calculate the RPKM for each sample in CNR_target object
 for(i in 1:length(CNR_target)){
   rpkm = bamFileRPKM(CNR_target[i])
   bamList[[basename(CNR_target[i])]] = rpkm %>% as.data.frame(check.names = FALSE) %>% 
     mutate(ID = paste0(seqnames,'_',start,"_",end))
   
 }
+########################################################################################################################
 
 # Read CG or CA methylation BSmap file
 meth  = readBismarkCoverage(mCA_target,assembly="unknown",min.cov=5,sample.id = "CA")
@@ -136,6 +144,9 @@ meth_at_target.df = getData(meth_at_target)
 meth_at_target.df$mCA = meth_at_target.df$numCs/meth_at_target.df$coverage
 meth_at_target.df$ID = paste0(meth_at_target$chr,'_',meth_at_target.df$start,"_",meth_at_target.df$end)
 
+
+########################################################################################################################
+# merge methylation and RPKM values for each gene coordiantes
 meth_cnr_merged.list = list()
 for(i in 1:length(bamList)){
   meth_cnr = merge(bamList[[i]],meth_at_target.df,by='ID') %>% 
@@ -148,7 +159,9 @@ meth_cnr_merged.list = lapply(meth_cnr_merged.list, function(x){ x %>% mutate_if
 meth_cnr_merged.list = lapply(meth_cnr_merged.list, function(x) x %>% 
                                 arrange(desc(mCA)) %>% filter(.[,1] >0))
 
+########################################################################################################################
 
+# calculate average RPKM and mCA of genes in selected bin and step size
 tmp = lapply(meth_cnr_merged.list, function(x) 
   summarize_logFC_mCA(x %>% as.data.frame(),binSize = binSize, stepSize = stepSize))
 tmp1 = lapply(tmp, function(x) do.call(rbind,x))
@@ -156,12 +169,15 @@ tmp2 = plyr::ldply(tmp1,data.frame)
 colnames(tmp2)[1] = 'CNR'
 meth_cnr_merged.avg = tmp2[,c(2,3,4,1)]
 
+
 # calculate pearson correlation between mCA and MECP2 on geneBody
 # mCA_cnr.corr.df = rbindlist(lapply(
-#  lapply(meth_cnr_merged.avg, function(x) cor.test(x[,1],x[,2])), tidy),#
-#  use.names = TRUE, idcol = 'Sample')
+# lapply(meth_cnr_merged.avg, function(x) cor.test(x[,1],x[,2])), tidy),#
+# use.names = TRUE, idcol = 'Sample')
 
-# Plot mCA vs CNR 
+
+########################################################################################################################
+# Plot mCA vs CNR  (use below script to customize the plot)
 cor.plot = meth_cnr_merged.avg %>% 
   #mutate(CNR= gsub('.final.sort.bam','',CNR)) %>% 
   #mutate(CNR=case_when(grepl('18',CNR) ~ '230405_18_WT_MECP2', grepl('13',CNR)~'230405_13_S835A_MECP2',TRUE ~ CNR)) %>% 
